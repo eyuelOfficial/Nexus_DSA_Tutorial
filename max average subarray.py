97fb2755db5e39dcd8615685a7f8530e22deb789
Eyuel Wondimagegn <eyuel.ugr-0655-17@aau.edu.et>
2026-02-19 20:21:01 +0300

    Add findMaxAverage method to calculate max average
    Implement a method to find the maximum average of a subarray.

---
+++ max average subarray.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        max_sum = sum(nums[:k])
+        cur_sum = sum(nums[:k])
+        for i in range(k , len(nums)):
+            cur_sum += nums[i]
+            cur_sum -= nums[i - k ]
+            max_sum = max(cur_sum , max_sum)
+        print (max_sum)
+        return max_sum / k 
+
